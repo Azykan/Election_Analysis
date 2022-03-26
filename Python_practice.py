85166fe6d1f93db1f60730aebb6ba0c6522d8324
azykan <angelazykan@gmail.com>
2022-03-26 01:51:35 -0500

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,18 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+for county in counties:
+     print(county)
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
+for num in range(5):
+    print(num)
+for i in range(len(counties)):
+    print(counties[1])
+for i in len(counties_tuples):
+    print(counties_tuples[i])
+for county in counties_tuples:
+    print(counties)
+for county in counties_tuples:
+    print(county)
+for i in len(counties_tuples):
+    print(counties_tuples[i])
